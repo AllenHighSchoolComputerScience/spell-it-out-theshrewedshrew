aaa6c159331bcf8c69e1bcf396923503769c361a
allencompsci <Taylor_Hudson@allenisd.org>
2017-09-07 15:21:11 -0500

    Update

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    int value;
+    String words [] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
+            "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen",
+            "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninty", "hundred", "thousand", "million", "billion"
+    };
+    public static void main(String[] args) {
+
+        
+
+        }
+}
